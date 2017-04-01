2141b3263fb3951239509231fdb6b2dd0629a867
Kim HyunWoo <stories2@gimhyeon-uui-MacBook-Pro.local>
2017-04-02 01:13:42 +0900

    Project Setting

---
+++ TrollLanguage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TrollLanguage
+//
+//  Created by 김현우 on 2017. 4. 1..
+//  Copyright © 2017년 김현우. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
